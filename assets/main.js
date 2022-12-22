28686187dec0e91294bcec170fa7c45e77fac5dc
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-22 15:47:42 +0100

    pls

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@ import { jourMeteo } from "../modules/jourMeteo.js";
 import { setUpButtons } from "../modules/setupButtons.js";
 
 export async function weatherMe(city){
-    let meteo = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=77cf7509f6657d267e637e6c2a540ddf`);
+    let meteo = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=77cf7509f6657d267e637e6c2a540ddf`);
     let meteofetched = await meteo.json();
     displayMeteo(meteofetched);
     console.log("requête envoyée");
