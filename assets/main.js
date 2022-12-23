651a840f3ba40424884e45c47dbda7ac225d45de
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-23 15:51:47 +0100

    fixing https

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,7 @@ import { setUpButtons } from "../modules/setupButtons.js";
 
 export async function weatherMe(city) {
   let meteo = await fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=77cf7509f6657d267e637e6c2a540ddf`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=77cf7509f6657d267e637e6c2a540ddf`
   );
   let meteofetched = await meteo.json();
   displayMeteo(meteofetched);
